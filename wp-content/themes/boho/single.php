3e48f3d11be05a8306b749ec4ae43e84b2754055
Michael Emerson <mjemerson@icloud.com>
2014-05-26 21:18:11 +0100

    Removed comments box and author box

--- wp-content/themes/boho/single.php
+++ wp-content/themes/boho/single.php
@@ -7,7 +7,7 @@
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
 <div class="post-title"><a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></div>
-<div class="post-date"><?php _e('Posted on ', 'newgamer'); ?><a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><?php the_time(get_option('date_format').', '.get_option('time_format')) ?></a><?php _e(' By ', 'newgamer'); ?><?php the_author_posts_link(); ?></div>
+<div class="post-date"><?php _e('Posted on ', 'newgamer'); ?><a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><?php the_time(get_option('date_format').', '.get_option('time_format')) ?></a></div>
 
 <?php if ( has_post_thumbnail()) : ?>
    <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" >
@@ -19,14 +19,6 @@
 
 <?php the_content('Read More...'); ?>
 
-</div>
-
-<div class="authortek"> 
-<?php echo get_avatar( get_the_author_meta( 'ID' ), 100 ); ?>
-<div class="authortekinfo">
-<h3><?php _e('About', 'newgamer'); ?> <?php the_author_posts_link(); ?></h3>
-<p><?php the_author_meta('description'); ?></p>
-</div>
 </div>
 
 <div class="post-navigation">
@@ -42,7 +34,6 @@
 
 <?php wp_link_pages(array('next_or_number'=>'next', 'previouspagelink' => '&#8592;', 'nextpagelink'=>'&#8594;')); ?>
 
-<?php comments_template(); ?>
 
 </div>
 
