3dda08ff5865fd934a137ad9930e76b86836eed5
Michael Emerson <mjemerson@icloud.com>
2014-06-13 22:25:24 +0100

    Moved gallery template

---
+++ wp-content/themes/boho/page-templates/gallery-main.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Template Name: Main Gallery Page
+ */
+
+get_header(); ?>
+
+    <div id="main">
+
+        <div id="content">
+
+            <h1>Galleries</h1>
+
+            <p>Click on the links below to view our galleries.</p>
+
+            <?php
+
+                $child_pages = wp_list_pages( 'sort_column=menu_order&title_li=&child_of=18&echo=0' );
+
+            print_r( $child_pages );
+
+
+            ?>
+
+
+        </div>
+
+        <?php get_sidebar(); ?>
+
+    </div>
+
+    <div class="delimiter"></div>
+
+<?php get_footer(); ?>
